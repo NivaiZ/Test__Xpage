a0386362c5b8e6f72109a02b8b31ed179e9106e4
ilya <nivaiz54@gmail.com>
2022-05-07 00:57:48 +0500

    test7

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -14,3 +14,18 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
   },
 });
+
+const swiperCollection = new Swiper('.collection-block__swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+  },
+});
